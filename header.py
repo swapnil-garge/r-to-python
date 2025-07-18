5ec82d3c8aba2ec589baa9cf155911ac54ea4b7f
Swapnil Garge <75273068+swapnil-garge@users.noreply.github.com>
2025-07-18 13:16:21 -0700

    Update header.py

--- header.py
+++ header.py
@@ -1,68 +1,43 @@
-# app.py
+# This is the corrected UI definition for app.py
 
-# ... other imports
-from shiny import App, ui
-
-# ... other code
+# The essential HTML for the EPA banner
+epa_banner_html = """
+<header class='masthead clearfix' role='banner'>
+     <img alt='' class='site-logo' src='https://www.epa.gov/sites/all/themes/epa/logo.png'>
+     <div class='site-name-and-slogan'>
+         <h1 class='site-name'><a href='https://www.epa.gov' rel='home' title='Go to the home page'><span>US EPA</span></a></h1>
+         <div class='site-slogan'>United States Environmental Protection Agency</div>
+     </div>
+</header>
+"""
 
 app_ui = ui.page_fluid(
-    # This ui.HTML() block contains the complete EPA banner and navigation structure
-    ui.HTML("""
-    <header class='masthead clearfix' role='banner'>
-         <img alt='' class='site-logo' src='https://www.epa.gov/sites/all/themes/epa/logo.png'>
-         <div class='site-name-and-slogan'>
-         <h1 class='site-name'><a href='https://www.epa.gov' rel='home' title='Go to the home page'><span>US EPA</span></a></h1>
-         <div class='site-slogan'>
-         United States Environmental Protection Agency
-         </div>
-         </div>
-         <div class='region-header'>
-         <div class='block-epa-core-gsa-epa-search' id='block-epa-core-gsa-epa-search'>
-         </div>
-         </div>
-    </header>
-    <nav class='nav main-nav clearfix' role='navigation'>
-         <div class='nav__inner'>
-         <h2 class='element-invisible'>Main menu</h2>
-         <ul class='menu' role='menu'>
-         <li class='expanded active-trail menu-item' role='presentation'>
-         <a class='active-trail menu-link' href='https://www.epa.gov/environmental-topics' role='menuitem' title='View links to the most popular pages for each of EPA&#8217s top environmental topics.'>Environmental Topics</a></li>
-         <li class='menu-item' role='presentation'>
-         <a class='menu-link' href='https://www.epa.gov/laws-regulations' role='menuitem' title='View links to regulatory information by topic and sector, and to top pages about environmental laws, regulations, policies, compliance, and enforcement.'>Laws &amp; Regulations</a></li>
-         <li class='expanded menu-item' role='presentation'>
-         <a class='menu-link' href='https://www.epa.gov/aboutepa' role='menuitem' title='Learn more about our mission, organization, and locations.'>About EPA</a></li>
-         </ul>
-         </div>
-    </nav>
-    <div class='mobile-nav' id='mobile-nav'>
-         <div class='mobile-bar clearfix'>
-         <label class='menu-button' for='mobile-nav-toggle'>Menu</label>
-         </div><input checked id='mobile-nav-toggle' type='checkbox'>
-         <div class='mobile-links element-hidden' id='mobile-links' style='height:2404px;'>
-         <ul class='mobile-menu'>
-         <li class='expanded menu-item'><a class='menu-link' href='https://www.epa.gov/environmental-topics' tabindex='-1' title='View links to the most popular pages for each of EPA&#8217s top environmental topics.'>Environmental Topics</a></li>
-         <li class='menu-item'><a class='menu-link' href='https://www.epa.gov/laws-regulations' tabindex='-1' title='View links to regulatory information by topic and sector, and to top pages about environmental laws, regulations, policies, compliance, and enforcement.'>Laws &amp; Regulations</a></li>
-         <li class='expanded menu-item'><a class='menu-link' href='https://www.epa.gov/aboutepa' tabindex='-1' title='Learn more about our mission, organization, and locations.'>About EPA</a></li>
-         </ul>
-         </div>
-    </div>
-    <section class='main-content clearfix' id='main-content' lang='en' role='main' tabindex='-1'>
-         <div class='region-preface clearfix'>
-         <div class='block-views-revision-hublinks-block' id='block-views-revision-hublinks-block'>
-         <div class='view view-revision-hublinks view-id-revision_hublinks'>
-         <span class='related-info'><strong>Related Topics:</strong></span>
-         <ul class='menu pipeline'>
-         <li class='menu-item'><a href='https://www.epa.gov/environmental-topics'>Environmental Topics</a></li>
-         </ul>
-         </div>
-         </div>
-         <div class='block block-pane block-pane-epa-web-area-connect' id='block-pane-epa-web-area-connect'>
-         <ul class='menu utility-menu'>
-         <li class='menu-item'><a class='menu-link' href='https://www.epa.gov/water-research/forms/contact-us-about-water-research'>Contact Us</a></li>
-         </ul>
-         </div>
-         </div>
-         <div class='main-column clearfix'>"""),
+    # Add the CSS file from the 'www' directory
+    ui.tags.head(
+        ui.tags.link(rel="stylesheet", type="text/css", href="style.css")
+    ),
+
+    # 1. Display the EPA banner
+    ui.HTML(epa_banner_html),
+
+    # 2. Add the main page title, styled correctly
+    ui.div(
+        ui.h1("Granular Activated Carbon Model (Python Version)", class_="page-title"),
+        class_="main-column clearfix"
+    ),
     
-    # ... rest of your UI definition (shinyswatch.theme, ui.page_navbar, etc.)
+    # 3. Add the rest of your Shiny UI (theme and navbar)
+    shinyswatch.theme.lumen(),
+    ui.page_navbar(
+        ui.nav("Input",
+            # ... content of the Input tab
+        ),
+        ui.nav("Output",
+            # ... content of the Output tab
+        ),
+        ui.nav("Fitted Data",
+            # ... content of the Fitted Data tab
+        ),
+        title="GAC Adsorption Model"
+    )
 )
